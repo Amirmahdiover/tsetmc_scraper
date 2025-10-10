b19fc13713a7aba78c0576887019c8780df8f7d5
Amirmahdi manafi <amirmahdi.manafi111@gmail.com>
2025-10-10 21:23:16 +0330

    changing redis host to redis

--- app/cache.py
+++ app/cache.py
@@ -3,7 +3,7 @@ import json
 import time
 
 # Connect to local Redis server
-r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+r = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
 
 # TTL for caching (e.g. 2 minutes)
 CACHE_TTL = 120
